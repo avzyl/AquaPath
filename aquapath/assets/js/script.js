3f68c8715ac8c910de7b684c82131245fd026e53
Adonicavilla <villa2232782@mls.ceu.edu.ph>
2024-11-04 00:16:21 +0800

    update

--- aquapath/assets/js/script.js
+++ aquapath/assets/js/script.js
@@ -18,6 +18,86 @@ console.log(Constants.highwayCoordinates);
 console.log(Constants.carmenCoord);
 console.log(Constants.hanggaStCoord);
 console.log(Constants.forrestStCoord);
+console.log(Constants.TrvrStCoord);
+console.log(Constants.MchlStCoord);
+console.log(Constants.LnnlStCoord);
+console.log(Constants.ShrwdStCoord);
+console.log(Constants.ShlbyStCoord);
+console.log(Constants.StnlyStCoord);
+console.log(Constants.StvStCoord);
+console.log(Constants.SpncrStCoord);
+console.log(Constants.TrntnStCoord);
+console.log(Constants.TrcyStCoord);
+console.log(Constants.TmthyStCoord);
+console.log(Constants.TbbyStCoord);
+console.log(Constants.ThrmnStCoord);
+console.log(Constants.VncntStCoord);
+console.log(Constants.VrglStCoord);
+console.log(Constants.WrrnStCoord);
+console.log(Constants.WllcStCoord);
+console.log(Constants.WltrStCoord);
+console.log(Constants.WlkrStCoord);
+console.log(Constants.MchllStCoord);
+console.log(Constants.MrrsStCoord);
+console.log(Constants.GfryStCoord);
+console.log(Constants.MrryStCoord);
+console.log(Constants.MyrnStCoord);
+console.log(Constants.NthnStCoord );
+console.log(Constants.NvneStCoord);
+console.log(Constants.RdStCoord);
+console.log(Constants.RlphStCoord);
+console.log(Constants.PrkrStCoord);
+console.log(Constants.ScttStCoord);
+console.log(Constants.SnfrdStCoord);
+console.log(Constants.RbnStCoord);
+console.log(Constants.RlyStCoord);
+console.log(Constants.RcStCoord );
+console.log(Constants.PlStCoord);
+console.log(Constants.OrvllStCoord);
+console.log(Constants.ChnsyStCoord);
+console.log(Constants.LndsyStCoord);
+console.log(Constants.LnwdStCoord);
+console.log(Constants.CrsnStCoord);
+console.log(Constants.HltnStCoord);
+console.log(Constants.HwrdStCoord);
+console.log(Constants.HrshllStCoord);
+console.log(Constants.HrmnStCoord);
+console.log(Constants.HlthStCoord);
+console.log(Constants.HywdStCoord);
+console.log(Constants.HrldStCoord);
+console.log(Constants.JsprStCoord);
+console.log(Constants.LslyStCoord);
+console.log(Constants.FlydStCoord);
+console.log(Constants.ElwdStCoord);
+console.log(Constants.ErnstStCoord);
+console.log(Constants.EthnStCoord);
+console.log(Constants.EllswrthStCoord);
+console.log(Constants.EllsStCoord);
+console.log(Constants.ErlStCoord);
+console.log(Constants.HntrStCoord);
+console.log(Constants.JrrtStCoord);
+console.log(Constants.CmrnStCoord);
+console.log(Constants.KrbyStCoord);
+console.log(Constants.LvrnStCoord);
+console.log(Constants.KrtsStCoord);
+console.log(Constants.KvnStCoord);
+console.log(Constants.KnnStCoord);
+console.log(Constants.KndllStCoord);
+console.log(Constants.KthStCoord);
+console.log(Constants.KnnthStCoord);
+console.log(Constants.KndrckStCoord);
+console.log(Constants.KnnyStCoord);
+console.log(Constants.JysnStCoord);
+console.log(Constants.JrmnStCoord );
+console.log(Constants.JrllStCoord);
+console.log(Constants.JstnStCoord);
+console.log(Constants.BrcStCoord);
+console.log(Constants.MxwllStCoord);
+console.log(Constants.MynrdStCoord);
+console.log(Constants.McknlyStCoord);
+console.log(Constants.MrrllStCoord);
+console.log(Constants.mckyStCoord);
+console.log(Constants.RndllStCoord);
 console.log(Constants.McArthurVillageMainRCoord);
 console.log(Constants.MAVSt1Coord);
 console.log(Constants.MAVSt2Coord);
@@ -39,7 +119,7 @@ console.log(Constants.calumpangLongosRdCoord);
 console.log(Constants.riversidestreetCoord);
 console.log(Constants.sanjoseVillageRdCoord);
 console.log(Constants.riversidestreet2Coord);
-console.log(Constants.drmCrstCoord);
+
 
 
 // polylines
@@ -47,7 +127,90 @@ let boundariesLine = L.polyline(Constants.boundaries, { color: 'black', weight: 
 let highwayLine = L.polyline(Constants.highwayCoordinates, { color: 'green', weight: 5 }).addTo(map);
 let carmenLine = L.polyline(Constants.carmenCoord, { color: 'yellow', weight: 5 }).addTo(map);
 let hanggaStLine = L.polyline(hanggaStCoord, { color: 'yellow', weight: 5 }).addTo(map);
-let forrestStLine = L.polyline(forrestStCoord, {color: 'red', weight: 10}).addTo(map);
+let drmCrstPrvLine = L.polyline(drmCrstPrvCoord, {color: 'red', weight:10}).addTo(map)
+let mckyStLine = L.polyline(mckyStCoord, {color: 'red', weight:10}).addTo(map)
+let RndllStLine = L.polyline(RndllStCoord, {color: 'red', weight:10}).addTo(map)
+let forrestStLine = L.polyline(forrestStCoord, {color: 'red', weight: 10}).addTo(map)
+let TrvrStStLine = L.polyline(TrvrStCoord, {color: 'red', weight: 10}).addTo(map)
+let MchlStStLine = L.polyline(MchlStCoord, {color: 'red', weight: 10}).addTo(map)
+let LnnlStLine = L.polyline(LnnlStCoord, {color: 'red', weight: 10}).addTo(map)
+let ShrwdStLine = L.polyline(ShrwdStCoord, {color: 'red', weight: 10}).addTo(map)
+let ShwnStLine = L.polyline(ShwnStCoord, {color: 'red', weight: 10}).addTo(map)
+let ShlbyStLine = L.polyline(ShlbyStCoord, {color: 'red', weight: 10}).addTo(map)
+let StnlyStLine = L.polyline(StnlyStCoord, {color: 'red', weight: 10}).addTo(map)
+let StvStLine = L.polyline(StvStCoord, {color: 'red', weight: 10}).addTo(map)
+let SpncrStLine = L.polyline(SpncrStCoord, {color: 'red', weight: 10}).addTo(map)
+let TrntnStLine = L.polyline(TrntnStCoord, {color: 'red', weight: 10}).addTo(map)
+let TrmnStLine = L.polyline(TrmnStCoord, {color: 'red', weight: 10}).addTo(map)
+let TrcyStLine = L.polyline(TrcyStCoord, {color: 'red', weight: 10}).addTo(map)
+let TmthyStLine = L.polyline(TmthyStCoord, {color: 'red', weight: 10}).addTo(map)
+let TbbyStLine = L.polyline(TbbyStCoord, {color: 'red', weight: 10}).addTo(map)
+let ThrmnStLine = L.polyline(ThrmnStCoord, {color: 'red', weight: 10}).addTo(map)
+let VrglStLine = L.polyline(VrglStCoord, {color: 'red', weight: 10}).addTo(map)
+let VncntStLine = L.polyline(VncntStCoord, {color: 'red', weight: 10}).addTo(map)
+let WllcStLine = L.polyline(WllcStCoord, {color: 'red', weight: 10}).addTo(map)
+let WrrnStLine = L.polyline(WrrnStCoord, {color: 'red', weight: 10}).addTo(map)
+let WltrStLine = L.polyline(WltrStCoord, {color: 'red', weight: 10}).addTo(map)
+let WlkrStLine = L.polyline(WlkrStCoord, {color: 'red', weight: 10}).addTo(map)
+let MchllStLine = L.polyline(MchllStCoord, {color: 'red', weight: 10}).addTo(map)
+let MrrsStLine =  L.polyline(MrrsStCoord, {color: 'red', weight: 10}).addTo(map)
+let GfryStLine =  L.polyline(GfryStCoord, {color: 'red', weight: 10}).addTo(map)
+let MrryStLine =  L.polyline(MrryStCoord, {color: 'red', weight: 10}).addTo(map)
+let MrynStLine =  L.polyline(MyrnStCoord, {color: 'red', weight: 10}).addTo(map)
+let NthnStLine =  L.polyline(NthnStCoord, {color: 'red', weight: 10}).addTo(map)
+let NvneStLine =  L.polyline(NvneStCoord, {color: 'red', weight: 10}).addTo(map)
+let RdStLine =  L.polyline(RdStCoord, {color: 'red', weight: 10}).addTo(map)
+let ScttStLine =  L.polyline(ScttStCoord, {color: 'red', weight: 10}).addTo(map)
+let SnfrdStLine =  L.polyline(SnfrdStCoord, {color: 'red', weight: 10}).addTo(map)
+let RbnStLine =  L.polyline(RbnStCoord, {color: 'red', weight: 10}).addTo(map)
+let RlyStLine =  L.polyline(RlyStCoord, {color: 'red', weight: 10}).addTo(map)
+let RlphStLine =  L.polyline(RlphStCoord, {color: 'red', weight: 10}).addTo(map)
+let PrkrStLine =  L.polyline(PrkrStCoord, {color: 'red', weight: 10}).addTo(map)
+let RcStLine =  L.polyline(RcStCoord, {color: 'red', weight: 10}).addTo(map)
+let PlStLine =  L.polyline(PlStCoord, {color: 'red', weight: 10}).addTo(map)
+let OrvllStLine =  L.polyline(OrvllStCoord, {color: 'red', weight: 10}).addTo(map)
+let ChnsyStLine =  L.polyline(ChnsyStCoord, {color: 'red', weight: 10}).addTo(map)
+let LndsyStLine =  L.polyline(LndsyStCoord, {color: 'red', weight: 10}).addTo(map)
+let CrsnStLine =  L.polyline(CrsnStCoord, {color: 'red', weight: 10}).addTo(map)
+let LnwdStLine =  L.polyline(LnwdStCoord, {color: 'red', weight: 10}).addTo(map)
+let HltnStLine =  L.polyline(HltnStCoord, {color: 'red', weight: 10}).addTo(map)
+let HwrdStLine =  L.polyline(HwrdStCoord, {color: 'red', weight: 10}).addTo(map)
+let HrshllStLine =  L.polyline(HrshllStCoord, {color: 'red', weight: 10}).addTo(map)
+let HrmnStLine =  L.polyline(HrmnStCoord, {color: 'red', weight: 10}).addTo(map)
+let HlthStLine =  L.polyline(HlthStCoord, {color: 'red', weight: 10}).addTo(map)
+let HywdStLine =  L.polyline(HywdStCoord, {color: 'red', weight: 10}).addTo(map)
+let HrldStLine =  L.polyline(HrldStCoord, {color: 'red', weight: 10}).addTo(map)
+let JsprStLine =  L.polyline(JsprStCoord, {color: 'red', weight: 10}).addTo(map)
+let LslyStLine =  L.polyline(LslyStCoord, {color: 'red', weight: 10}).addTo(map)
+let FlydStLine =  L.polyline(FlydStCoord, {color: 'red', weight: 10}).addTo(map)
+let ElwdStLine =  L.polyline(ElwdStCoord, {color: 'red', weight: 10}).addTo(map)
+let ErnstStLine =  L.polyline(ErnstStCoord, {color: 'red', weight: 10}).addTo(map)
+let EthnStLine =  L.polyline(EthnStCoord, {color: 'red', weight: 10}).addTo(map)
+let EllswrthStLine =  L.polyline(EllswrthStCoord, {color: 'red', weight: 10}).addTo(map)
+let EllsStLine =  L.polyline(EllsStCoord, {color: 'red', weight: 10}).addTo(map)
+let ErlStLine =  L.polyline(ErlStCoord, {color: 'red', weight: 10}).addTo(map)
+let CmrnStLine =  L.polyline(CmrnStCoord, {color: 'red', weight: 10}).addTo(map)
+let HntrStLine =  L.polyline(HntrStCoord, {color: 'red', weight: 10}).addTo(map)
+let JrrtStLine =  L.polyline(JrrtStCoord, {color: 'red', weight: 10}).addTo(map)
+let KrbyStLine =  L.polyline(KrbyStCoord, {color: 'red', weight: 10}).addTo(map)
+let LvrnStLine =  L.polyline(LvrnStCoord, {color: 'red', weight: 10}).addTo(map)
+let KrtsStLine =  L.polyline(KrtsStCoord, {color: 'red', weight: 10}).addTo(map)
+let KvnStLine =  L.polyline(KvnStCoord, {color: 'red', weight: 10}).addTo(map)
+let BrcStLine =  L.polyline(BrcStCoord, {color: 'red', weight: 10}).addTo(map)
+let MxwllStLine =  L.polyline(MxwllStCoord, {color: 'red', weight: 10}).addTo(map)
+let MynrdStLine =  L.polyline(MynrdStCoord, {color: 'red', weight: 10}).addTo(map)
+let McknlyStLine =  L.polyline(McknlyStCoord, {color: 'red', weight: 10}).addTo(map)
+let MrrllStLine =  L.polyline(MrrllStCoord, {color: 'red', weight: 10}).addTo(map)
+let KnnStLine =  L.polyline(KnnStCoord, {color: 'red', weight: 10}).addTo(map)
+let KndllStLine =  L.polyline(KndllStCoord, {color: 'red', weight: 10}).addTo(map)
+let KthStLine =  L.polyline(KthStCoord, {color: 'red', weight: 10}).addTo(map)
+let JstnStLine =  L.polyline(JstnStCoord, {color: 'red', weight: 10}).addTo(map)
+let KndrckStLine =  L.polyline(KndrckStCoord, {color: 'red', weight: 10}).addTo(map)
+let JysnStLine =  L.polyline(JysnStCoord, {color: 'red', weight: 10}).addTo(map)
+let JrmnStLine =  L.polyline(JrmnStCoord, {color: 'red', weight: 10}).addTo(map)
+let JrllStLine =  L.polyline(JrllStCoord, {color: 'red', weight: 10}).addTo(map)
+let KnnyStLine =  L.polyline(KnnyStCoord, {color: 'red', weight: 10}).addTo(map)
+let KnnthStLine =  L.polyline(KnnthStCoord, {color: 'red', weight: 10}).addTo(map)
 let McArthurVillageMainRLine = L.polyline(McArthurVillageMainRCoord, {color: 'green', weight:10}).addTo(map);
 let MAVSt1Line = L.polyline(MAVSt1Coord, {color: 'yellow', weight:10}).addTo(map);
 let MAVSt2Line = L.polyline(MAVSt2Coord, {color: 'yellow', weight:10}).addTo(map);
@@ -69,7 +232,7 @@ let calumpangLongosRdLine = L.polyline(calumpangLongosRdCoord, {color: 'yellow',
 let riversidestreetLine = L.polyline(riversidestreetCoord, {color: 'yellow', weight:10}).addTo(map);
 let sanjoseVillageRdLine = L.polyline(sanjoseVillageRdCoord, {color: 'orange', weight:10}).addTo(map);
 let riversidestreet2Line = L.polyline(riversidestreet2Coord, {color: 'green', weight:10}).addTo(map);
-let drmCrstLine = L.polyline(drmCrstCoord, {color: 'blue', weight:10}).addTo(map);
+
 
 // bind the popup
 const waterLevel = "<?php echo htmlspecialchars($waterLevel); ?>";
